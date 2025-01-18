f93021dc236a7c973ae10eadec190ae2ea2e4628
joeyvan <84177114+joeyvanJB@users.noreply.github.com>
2025-01-18 23:47:27 +0800

    refactor: optimize DOM manipulation and improve CSS transitions

--- popup.js
+++ popup.js
@@ -1,98 +1,75 @@
 $(document).ready(function() {
+  /*
+   * Cache DOM elements for better performance
+   */
+  const $input = $("#input");
+  const $tabs = $("#tabs li");
+  const $inputPasswordWrapper = $("#input-password-wrapper");
+  const $buttons2 = $(".buttons-2");
+  const $screen1 = $("#screen-1");
+  const $screen2 = $("#screen-2");
 
-/*
- * I decided to leave popout button on the popout page
- *
-  if (typeof chrome.extension != "undefined") {
-    if (chrome.extension.getBackgroundPage().separatePopup == true) {
-      $("#popup").hide();
-    } else {
-      $("#popup").show();
-    }
-  }
-*/  
-  
+
   /*
    * Events registration
   */
-  $("#input").keyup(function () {
-    hasher.update();
-  });
-  $("#input").change(function () {
-    hasher.update();
+  $input.on("keyup change", function () {
+      hasher.update();
   });
 
   // Open separate window (pop-out)
   $("#button-popout").click(function () {
-    if (typeof chrome.extension != "undefined") {
-      //chrome.extension.getBackgroundPage().separatePopup = true;
-/*      
-      chrome.windows.create({
-        url: 'popup.html',
-        type: 'popup',
-        width: 700,
-        height: 800
-      });
-*/      
+  if (typeof chrome.extension != "undefined") {
       chrome.tabs.create({
-        url: 'popup.html'
+      url: 'popup.html'
       });
-    }
+  }
   });
 
   // Click on tab (Hash/HMAC/...)
-  $("#tabs li").click(function () {
-    // highlight active tab, remove highlight on everything else
-    $("#tabs li").removeClass("on");
-    $(this).addClass("on");
+  $tabs.click(function () {
+      // highlight active tab, remove highlight on everything else
+      $tabs.removeClass("on");
+      $(this).addClass("on");
 
-    // show/hide optional fields
-    if (tabs[this.id] == tabs.hmac || tabs[this.id] == tabs.cipher) {
-      $("#input-password-wrapper").show();
-    } else {
-      $("#input-password-wrapper").hide();
-    }
+      // show/hide optional fields
+      if (tabs[this.id] == tabs.hmac || tabs[this.id] == tabs.cipher) {
+          $inputPasswordWrapper.show();
+      } else {
+          $inputPasswordWrapper.hide();
+      }
 
-    hasher.tab = tabs[this.id];
-    hasher.init();
-    hasher.update();
-    $("#input-value").focus();
+      hasher.tab = tabs[this.id];
+      hasher.init();
+      hasher.update();
+      $("#input-value").focus();
   });
   
   /*
-   * Animations
+   * Animations (using CSS transitions instead of jQuery animate)
    */
-  $(".buttons-2").mouseenter(function(){
-    $(this).animate(
-      {
-        opacity: 0.8
-      },
-      150
-    );
-  });
-  $(".buttons-2").mouseleave(function(){
-    $(this).animate(
-      {
-        opacity: 0.4
-      },
-      300
-    );
+  $buttons2.on("mouseenter", function(){
+      $(this).addClass("hovered");
   });
   
-  
+  $buttons2.on("mouseleave", function(){
+      $(this).removeClass("hovered");
+  });
+
   /*
    * Hash navigation
    */
-  onHashChange = function () {
-    var hash = window.location.hash.slice(1)
-    $(".screens").hide();
-    if (hash == "info") {
-      $("#screen-2").show().scrollTop();
-    } else {
-      $("#screen-1").show().scrollTop();
-    }
-  }
-  $(window).bind('hashchange', onHashChange);  
+  const onHashChange = function () {
+      const hash = window.location.hash.slice(1);
+      $(".screens").hide();
+      if (hash === "info") {
+          $screen2.show().scrollTop();
+      } else {
+          $screen1.show().scrollTop();
+      }
+  };
+
+  $(window).on('hashchange', onHashChange);  
 
   /*
    * Init
@@ -103,6 +80,5 @@ $(document).ready(function() {
   
   // Focus hack, see http://stackoverflow.com/a/11400653/1295557
   if (location.search != "?focusHack") location.search = "?focusHack";
-  //$("#input-value").focus();
   window.scrollTo(0, 0);
 });
